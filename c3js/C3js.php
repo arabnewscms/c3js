2f27c6d14aab3f702327fe98cc1332f3bc1fac63
php anonymous <php.anonymous1@gmail.com>
2020-06-27 21:50:38 +0200

    Update C3js.php

--- c3js/C3js.php
+++ c3js/C3js.php
@@ -1,34 +1,28 @@
 <?php
 namespace Phpanonymous\C3JS;
-
 use Illuminate\Support\ServiceProvider;
 
-class C3JSProvider extends ServiceProvider {
+class C3js extends ServiceProvider {
 
-	/**
-	 * Bootstrap services.
-	 *
-	 * @return void
-	 */
-
-	public function boot() {
-
-		$this->publishes([__DIR__ .'/js'  => base_path('public')]);
-		$this->publishes([__DIR__ .'/css' => base_path('public')]);
-
+	public static function c3jAsset() {
+		$asset = '';
+		$asset .= '<link href="'.url('css/c3js/c3.css').'" rel="stylesheet">';
+		$asset .= '<script src="'.url('js/c3js/c3.min.js').'"></script>';
+		$asset .= '<script src="'.url('js/c3js/d3.v5.min.js').'"></script>';
+		return $asset;
 	}
 
-	/**
-	 * Register services.
-	 *
-	 * @return void
-	 */
-	public function register() {
+	public static function widget($data, $chart_name = null) {
+
+		$data       = json_encode($data);
+		$chart_name = $chart_name === null?'#chart_'.rand(0000, 999999):$chart_name;
+		$var_name   = rand(0000, 999999);
+
+		$c3jdata = '';
+		$c3jdata .= '<div id="'.$chart_name.'"></div>';
+		$c3jdata .= '<script type="text/javascript">';
+		$c3jdata .= 'var chart_'.$var_name.' = c3.generate({bindto: "'.$chart_name.'",'.$data.'});';
+		$c3jdata .= '</script>';
 
 	}
-
-	public function provides() {
-
-	}
-
 }
