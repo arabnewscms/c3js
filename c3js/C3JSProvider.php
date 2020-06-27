d4412594f43325d002ca8e570e4ca948a4bdd47d
php anonymous <php.anonymous1@gmail.com>
2020-06-27 22:37:54 +0200

    Update C3JSProvider.php

--- c3js/C3JSProvider.php
+++ c3js/C3JSProvider.php
@@ -13,8 +13,8 @@ class C3JSProvider extends ServiceProvider {
 
 	public function boot() {
 
-		$this->publishes([__DIR__ .'/js'  => base_path('public')]);
-		$this->publishes([__DIR__ .'/css' => base_path('public')]);
+		$this->publishes([__DIR__ .'/js'  => base_path('public/js')]);
+		$this->publishes([__DIR__ .'/css' => base_path('public/css')]);
 
 	}
 
