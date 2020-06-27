bfa01800ce41706f790a98654732cef66a2e24de
php anonymous <php.anonymous1@gmail.com>
2020-06-27 21:50:49 +0200

    Update C3JSProvider.php

--- c3js/C3JSProvider.php
+++ c3js/C3JSProvider.php
@@ -13,10 +13,8 @@ class C3JSProvider extends ServiceProvider {
 
 	public function boot() {
 
-		if (!file_exists(base_path('config').'/itconfiguration.php')) {
-			$this->publishes([__DIR__ .'/js'  => base_path('public')]);
-			$this->publishes([__DIR__ .'/css' => base_path('public')]);
-		}
+		$this->publishes([__DIR__ .'/js'  => base_path('public')]);
+		$this->publishes([__DIR__ .'/css' => base_path('public')]);
 
 	}
 
@@ -26,8 +24,7 @@ class C3JSProvider extends ServiceProvider {
 	 * @return void
 	 */
 	public function register() {
-		$this->app->make('Phpanonymous\C3JS\C3JSProvider');
-		$this->app->make('Phpanonymous\C3JS\C3js');
+
 	}
 
 	public function provides() {
