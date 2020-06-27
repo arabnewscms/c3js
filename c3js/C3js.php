009fa2de2faf1b525391fef959ca48c8cdb3e8b8
php anonymous <php.anonymous1@gmail.com>
2020-06-27 22:03:18 +0200

    Update C3js.php

--- c3js/C3js.php
+++ c3js/C3js.php
@@ -8,11 +8,11 @@ class C3js extends ServiceProvider {
 		$asset = '';
 		$asset .= '<link href="'.url('css/c3js/c3.css').'" rel="stylesheet">';
 		$asset .= '<script src="'.url('js/c3js/c3.min.js').'"></script>';
-		$asset .= '<script src="'.url('js/c3js/d3.v5.min.js').'"></script>';
+		$asset .= '<script src="'.url('js/c3js/d3.min.js').'"></script>';
 		return $asset;
 	}
 
-	public static function widget($data, $chart_name = null) {
+	public function widget($data, $chart_name = null) {
 
 		$data       = json_encode($data);
 		$chart_name = $chart_name === null?'#chart_'.rand(0000, 999999):$chart_name;
@@ -24,6 +24,5 @@ class C3js extends ServiceProvider {
 		$c3jdata .= 'var chart_'.$var_name.' = c3.generate({bindto: "'.$chart_name.'",'.$data.'});';
 		$c3jdata .= '</script>';
 		return $c3jdata;
-
 	}
 }
