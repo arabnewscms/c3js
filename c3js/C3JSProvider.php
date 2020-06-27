a03c3a8c0ce0994f76e6f0fa7465466ccce64021
php anonymous <php.anonymous1@gmail.com>
2020-06-27 21:20:21 +0200

    Add files via upload

---
+++ c3js/C3JSProvider.php
@@ -0,0 +1,37 @@
+<?php
+namespace Phpanonymous\C3JS;
+
+use Illuminate\Support\ServiceProvider;
+
+class C3JSProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+
+	public function boot() {
+
+		if (!file_exists(base_path('config').'/itconfiguration.php')) {
+			$this->publishes([__DIR__ .'/js'  => base_path('public')]);
+			$this->publishes([__DIR__ .'/css' => base_path('public')]);
+		}
+
+	}
+
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->app->make('Phpanonymous\C3JS\C3JSProvider');
+		$this->app->make('Phpanonymous\C3JS\C3js');
+	}
+
+	public function provides() {
+
+	}
+
+}
---
+++ c3js/C3js.php
@@ -0,0 +1,28 @@
+<?php
+namespace Phpanonymous\C3JS;
+use Illuminate\Support\ServiceProvider;
+
+class C3js extends ServiceProvider {
+
+	public static function c3jAsset() {
+		$asset = '';
+		$asset .= '<link href="'.url('css/c3js/c3.css').'" rel="stylesheet">';
+		$asset .= '<script src="'.url('js/c3js/c3.min.js').'"></script>';
+		$asset .= '<script src="'.url('js/c3js/d3.v5.min.js').'"></script>';
+		return $asset;
+	}
+
+	public function widget($data, $chart_name = null) {
+
+		$data       = json_encode($data);
+		$chart_name = $chart_name === null?'#chart_'.rand(0000, 999999):$chart_name;
+		$var_name   = rand(0000, 999999);
+
+		$c3jdata = '';
+		$c3jdata .= '<div id="'.$chart_name.'"></div>';
+		$c3jdata .= '<script type="text/javascript">';
+		$c3jdata .= 'var chart_'.$var_name.' = c3.generate({bindto: "'.$chart_name.'",'.$data.'});';
+		$c3jdata .= '</script>';
+
+	}
+}
